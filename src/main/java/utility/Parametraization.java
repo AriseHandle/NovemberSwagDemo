051052bd455897edc297c536ba4b86337a346883
ajaykhule0405 <ajayp.khule@gmail.com>
2024-03-25 22:18:11 +0530

    Parametraization method added

--- src/main/java/utility/Parametraization.java
+++ src/main/java/utility/Parametraization.java
@@ -1,16 +1,17 @@
 package utility;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Parametraization {
 	
-	public String getExcelData(String sheetname,int row,int cell) throws FileNotFoundException
+	public String getExcelData(String sheetname,int row,int cell) throws EncryptedDocumentException, IOException
 	{
 		FileInputStream file = new FileInputStream("F:\\Arise Java\\Project\\NovemberSwagDemo\\src\\test\\resources\\TestData.xlsx");
-		String value = WorkbookFactory.create(file).getSheet(sheetname).getRow(row).getCell(cell).getStringCellvalue();
+		String value = WorkbookFactory.create(file).getSheet(sheetname).getRow(row).getCell(cell).getStringCellValue();
 		return value;
 	}
 
