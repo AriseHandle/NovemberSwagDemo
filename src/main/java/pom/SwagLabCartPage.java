e96389a04c540261d420b57119c0c0ce89ddc64d
GaikwadShubh <shubhamgaikwadmaruti@gmail.com>
2024-04-01 19:21:59 +0530

    changes made as per comment

--- src/main/java/pom/SwagLabCartPage.java
+++ src/main/java/pom/SwagLabCartPage.java
@@ -11,11 +11,11 @@ public class SwagLabCartPage
 {
 	@FindBy(xpath="//button[@id='continue-shopping']")private WebElement continueShopping;
 	@FindBy(xpath="//button[@id='checkout']")private WebElement checkoutButton;	
-	@FindBy(xpath="//div[@data-test='inventory-item']")private List<WebElement> cartProductContainer; 
+	@FindBy(xpath="//div[@data-test='inventory-item']")private List<WebElement> productInCart; 
 	@FindBy(xpath="//div[@data-test='inventory-item']//div//a//div")private List<WebElement> cartProductName;
-	@FindBy(xpath="//div[@data-test='inventory-item-price']")private List<WebElement> cartProductPrice; ////div//div//div//div//div//div[@data-test='inventory-item-price']
+	@FindBy(xpath="//div[@data-test='inventory-item-price']")private List<WebElement> cartProductPrice; 
 	@FindBy(xpath="//div[@data-test='inventory-item-desc']")private List<WebElement> cartProductDescription;
-	//xpath = //*[contains(@class,"item")]//div[contains(@class,"label")]//div[contains(@class,"desc")]
+	
 	public SwagLabCartPage(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
