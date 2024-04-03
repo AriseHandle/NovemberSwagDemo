81a2e836c5b873ae028dd9e43c5a93d4ce16b659
chetaan17 <chetanpatidar123@gmail.com>
2024-04-03 18:29:59 +0530

    menutest added

---
+++ src/test/java/test/MenuTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import pojo.Browser;
+import pom.SwagLabHomePage;
+import utility.Parametrization;
+
+public class MenuTest extends BaseTest{
+
+	@BeforeMethod
+	public void OpenApplication() {
+		driver=Browser.launchBrowser();
+	}
+	
+	@Test
+	public void verifyAbout() {
+		user =Parametrization.getExcelData("Creds", 1, 1);
+		pass =Parametrization.getExcelData("Creds", 2, 1);
+		loginSwagLab(driver, "standard_user", "secret_sauce");
+		SwagLabHomePage swagLabHomePage =new SwagLabHomePage(driver);
+		swagLabHomePage.clickOnMenuButton();
+		swagLabHomePage.clickOnAboutButton();
+		String title=driver.getTitle();
+		Assert.assertEquals("Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing", title);
+	}
+	
+	@Test
+	public void verifyLogout() {
+		user =Parametrization.getExcelData("Creds", 1, 1);
+		pass =Parametrization.getExcelData("Creds", 2, 1);
+		loginSwagLab(driver, "standard_user", "secret_sauce");
+		SwagLabHomePage swagLabHomePage =new SwagLabHomePage(driver);
+		swagLabHomePage.clickOnMenuButton();
+		swagLabHomePage.clickOnlogoutButton();
+		String url=driver.getCurrentUrl();
+		Assert.assertEquals("https://www.saucedemo.com/", url);
+	}
+	
+}
