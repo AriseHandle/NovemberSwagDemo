6747e6f8fa94eb2ef34c832bfda10c4239958ab1
bhagya369 <bhagyashrib581@gmail.com>
2024-04-08 23:02:15 +0530

    added practice class

---
+++ src/test/java/test/Listener.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener{
+	
+		public void onTestStart(ITestResult result)
+		{
+			//
+		}
+		
+		public void onTestSuccess(ITestResult result)
+		{
+			//
+		}
+		
+		public void onTestFailure(ITestResult result)
+		{
+			//
+		}
+		
+		public void onTestSkipped(ITestResult result)
+		{
+			//
+		}
+	
+
+}
---
+++ src/test/java/test/PracticeClass.java
@@ -0,0 +1,8 @@
+package test;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		System.out.println("its a practice statement ");
+	}
+}
